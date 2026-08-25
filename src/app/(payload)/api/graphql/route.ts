a01abd62a7366bbbbec9fd1efcab13f26caa06f4
Rafael Chirinos <rafaelchirinos33@gmail.com>
2026-08-25 17:37:16 -0400

    Integración de Payload CMS

---
+++ src/app/(payload)/api/graphql/route.ts
@@ -0,0 +1,5 @@
+import configPromise from '@payload-config';
+import { GRAPHQL_PLAYGROUND_GET, GRAPHQL_POST } from '@payloadcms/next/routes';
+
+export const POST = GRAPHQL_POST(configPromise);
+export const GET = GRAPHQL_PLAYGROUND_GET(configPromise);
